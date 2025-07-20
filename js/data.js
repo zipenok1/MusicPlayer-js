dac9e2ebfd2ac331b45fb32f1d562a26ebe8d1f2
дмитрий <dmitrijkopejka5@gmail.com>
2025-07-20 22:51:22 +0300

    -

--- js/data.js
+++ js/data.js
@@ -1,7 +1,22 @@
 export const data = [
     {
-        id: 1,
-        track: 'How Much Is Weed?',
-        link: 'track1.mp3'
-    }
-]
+      id: 1,
+      link: "beat1.mp3",
+      track: "Enemy"
+    },
+    {
+      id: 2,
+      link: "beat2.mp3",
+      track: "Next to Me"
+    },
+    {
+      id: 3,
+      link: "beat3.mp3",
+      track: "Rammlied"
+    },
+    {
+      id: 4,
+      link: "beat4.mp3",
+      track: "The Search"
+    },
+  ];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,90 @@
 import { data } from "./data.js";
 
-console.log(data);
+const AudioController = {
+    state: {
+        aduios: [],
+        current: {}
+    },
+
+    init() {
+        this.initVariables()
+        this.renderAudios()
+        this.initEvent()
+    },
+
+    initVariables() {
+        this.audioList = document.querySelector('.items')
+        this.currentList = document.querySelector('.current')
+    },
+
+    initEvent() {
+        this.audioList.addEventListener('click', this.handleCl.bind(this))
+    },
+
+    renderCurrent(current){
+        const {link, track} = current
+        const [imges] = link.split('.')
+        const item =  `
+        <div class="current-img" style="background-image: url(img/${imges}.jpg)"></div>
+            <div class="current-info">
+                <div class="current-info__top">
+                    <h2>${track}</h2>
+                </div>
+                <div class="current-info__bt">
+                    <button>
+                        <img class="arrow-left" src="img/svg/fast.svg" alt="fast">
+                    </button>
+                    <button>
+                        <img src="img/svg/play.svg" alt="play">
+                    </button>
+                    <button>
+                        <img src="img/svg/fast.svg" alt="fast">
+                    </button>
+                </div>
+            <div class="current-info__progress"><span></span></div>
+        </div>
+        `
+        this.currentList.innerHTML = item
+    },
+
+    setCurrent(itemId){
+        const current = this.state.aduios.find(({id}) => +id === +itemId)
+        console.log(current);
+        if(!current) return
+
+        this.state.current = current
+        this.renderCurrent(current)
+    },
+
+    handleCl({target}){
+        const {id} = target.dataset
+        if(!id) return
+
+        this.setCurrent(id)
+    },
+
+    loadAudio(audio){
+        const { id, link, track } = audio
+        const [imges] = link.split('.')
+        const item = 
+        `
+        <div class="item" data-id="${id}">
+            <div class="item-img" style="background-image: url(img/${imges}.jpg)"></div>
+            <h3 class="item-track">${track}</h3>
+        </div>
+        `
+        this.audioList.innerHTML += item
+    },
+
+    renderAudios(){
+        data.forEach((item) => {
+            const audio = new Audio(`./audio/${item.link}`)
+            const newItem = {...item, audio}
+
+            this.state.aduios.push(newItem)
+            this.loadAudio(newItem)
+        })
+    }
+}
+
+AudioController.init()
